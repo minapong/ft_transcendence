ea6837810ceeef82193f45772135c83ff174ebb9
iamhashir <ihashirr@outlook.com>
2025-11-02 20:50:48 +0400

    folder struct

--- Frontend/src/layouts/MainLayout.tsx
+++ Frontend/src/layouts/MainLayout.tsx
@@ -1,9 +1,11 @@
 import { Header } from "components/Header";
+// import { Side } from "components/Side";
 
 export function MainLayout(Page: () => HTMLElement) {
 	return(
 		<div>
 			<Header/>
+			{/* <Side/> */}
 			<main id="spa-root">{Page()}</main>
 		</div>
 	);
