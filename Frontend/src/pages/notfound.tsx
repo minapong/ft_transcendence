d837398fc21be98da7d7abafaa3e8d556ca52f21
iamhashir <ihashirr@outlook.com>
2025-11-14 15:50:52 +0400

    Fix: updated 404 page after review

--- Frontend/src/pages/notfound.tsx
+++ Frontend/src/pages/notfound.tsx
@@ -1,3 +1,7 @@
 export default function NotFound() {
-  return <div className="w-screen h-screen flex justify-center items-center text-2xl text-white">404 - Page Not Found</div>;
+  return (
+    <div className="w-screen h-screen flex justify-center items-center text-2xl text-white">
+      404 - Page Not Found
+    </div>
+  );
 }
