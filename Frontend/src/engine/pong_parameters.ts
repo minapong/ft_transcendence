8e5a741a5959bd40dd8ba5ea0e8d2f51d8907e30
saherrer-git <saherrer@student.42abudhabi.ae>
2025-11-24 22:24:51 +0400

    updated the game logic with new variable and constants

--- Frontend/src/engine/pong_parameters.ts
+++ Frontend/src/engine/pong_parameters.ts
@@ -1,10 +1,17 @@
 export const GAME_WIDTH = 800;
 export const GAME_HEIGHT = 500;
+export const WALL_WIDTH = 8;
 
 export const BALL_SIZE = 16;
 
 export const PADDLE_HEIGHT = 96;
 export const PADDLE_SPEED = 6;
+export const PADDLE_WIDTH = 12;
+export const PLAYABLE_WIDTH = GAME_WIDTH - (2 * WALL_WIDTH);
+export const PLAYABLE_HEIGHT = GAME_HEIGHT - (2 * WALL_WIDTH);
+export const LEFT_PADDLE_X = 16;
+export const RIGHT_PADDLE_X = PLAYABLE_WIDTH - 16 - PADDLE_WIDTH;
+
 
 export const GAME_SPEED = 3;
 
