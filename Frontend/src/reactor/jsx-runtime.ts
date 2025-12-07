382e5222e833e43417445e509d7816f990767a7e
twillian <twillian.sc21@gmail.com>
2025-12-07 17:05:46 +0400

    update : rm reactor/jsx-runtime.ts as it is not used

--- Frontend/src/reactor/jsx-runtime.ts
+++
@@ -1,6 +0,0 @@
-export function jsx(type: any, props: any, key?: any) {
-	return { type, props, key };
-}
-
-export const jsxs = jsx;
-export const Fragment = (props: { children: any }) => props.children;
