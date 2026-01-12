7f352ddf835afda4ea5e32e559410cfee4b880c9
twillian <twillian.sc21@gmail.com>
2026-01-12 15:04:27 +0400

    update : remove old repositories and routes

--- Backend/src/repositories/user.repo.js
+++
@@ -1,8 +0,0 @@
-import { prisma } from '../db/prisma.js';
-export const UserRepo = {
-    create: (data) => prisma.user.create({ data }),
-    findById: (id) => prisma.user.findUnique({ where: { id } }),
-    findByEmail: (email) => prisma.user.findUnique({ where: { email } }),
-    update: (id, patch) => prisma.user.update({ where: { id }, data: patch }),
-    list: (take = 50, skip = 0) => prisma.user.findMany({ take, skip, orderBy: { created_at: 'desc' } })
-};
