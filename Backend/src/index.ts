45c3049736dd99b64ea2852fe3a86f1a32125664
Natalia Magdanova <nmagdano@lab3r8s8.42abudhabi.ae>
2026-01-27 17:04:18 +0400

    [ADD] : avatars settings repos , services

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -4,6 +4,7 @@ import jwt from "@fastify/jwt";
 
 import websocket from "@fastify/websocket";
 import fastifyStatic from "@fastify/static";
+import multipart from "@fastify/multipart";
 import path from "path";
 
 import { registerPresenceWs} from "./routes/presence.ws.js";
@@ -20,6 +21,8 @@ import { registerProfileRoutes } from "./routes/profile.js";
 import { registerMeRoutes } from "./routes/me.js";
 import { registerStatsRoutes } from "./routes/stats.js";
 import { registerFriendRoutes } from "./routes/friends.js";
+import { registerAvatarRoutes } from "./routes/avatar.routes.js";
+
 
 
 const server = Fastify({ logger: true });
@@ -47,6 +50,10 @@ async function start() {
       prefix: "/static/",
     });
 
+    await server.register(multipart, {
+      limits: { fileSize: 2 * 1024 * 1024 },
+    });
+
     registerPresenceWs(server);
     registerPresenceRoutes(server);
 
@@ -58,6 +65,8 @@ async function start() {
     registerMeRoutes(server);
     registerStatsRoutes(server);
     registerFriendRoutes(server);
+    registerAvatarRoutes(server);
+
 
     server.listen({ port: 3000, host: "0.0.0.0" }, (err, address) => {
       if (err){ process.exit(1); throw err; }
